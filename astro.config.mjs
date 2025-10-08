23c4149931509b9e068517db2b68830e4c7bad63
sg-fernando <45243662+sg-fernando@users.noreply.github.com>
2025-10-08 14:30:26 -0600

    vercel build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import preact from '@astrojs/preact';
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), preact()],
-  adapter: vercel()
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  adapter: vercel(),
 });
