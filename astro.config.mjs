38497ddf50c39dfda7a452047bb25adb466b8d7f
sg-fernando <45243662+sg-fernando@users.noreply.github.com>
2025-10-08 14:19:35 -0600

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import react from '@astrojs/react';
 
 import preact from '@astrojs/preact';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), preact()]
+  integrations: [react(), preact()],
+  adapter: vercel()
 });
