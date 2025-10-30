c5e0f9ab166724138c61f946f29fc655ac164fe1
sg-fernando <45243662+sg-fernando@users.noreply.github.com>
2025-10-30 10:47:43 -0600

    remove build .gitignore and new project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,9 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  adapter: vercel(),
+  integrations: [mdx(), sitemap()]
 });
---
+++ src/pages/projects/project-95.md
@@ -0,0 +1,8 @@
+---
+layout: ../../layouts/ProjectLayout.astro
+title: 'Pokemon Cards'
+pubDate: 'Oct 30, 2025'
+lastUpdated: 'Oct 30, 2025'
+description: 'Pokemon Card viewer. Built with React and using PokéAPI.'
+externalUrl: 'https://github.com/sg-fernando/pokemon-react/'
+---
